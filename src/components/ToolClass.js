c45b00470de8eae470fbb8851d8651ad2e3399d3
陆夏立 <luxiali@biosan.cn>
2018-05-30 15:27:28 +0800

    update

--- src/components/ToolClass.js
+++ src/components/ToolClass.js
@@ -152,7 +152,7 @@ ToolClass.listSelectKey= (json,name) => {
 ToolClass.GetQueryString=(name)=>{
     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
     var r = window.location.search.substr(1).match(reg);
-    if(r!=null)return  unescape(r[2]); return null;
+    if(r!=null)return   unescape(decodeURI( r[2])); return null;
 };
 
 //手机号 身份证加密
